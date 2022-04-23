8cc652f7ba6049f3ef9cd7a0dfc0537fa1a2d303
unknown <lgmaster26060@yandex.ru>
2022-04-23 22:49:36 +0300

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+<h2 align="center">Social Network project for MP </h2>
---
+++ prestart.sh
@@ -0,0 +1,8 @@
+#
+
+sleep 10;
+python manage.py migrate
+
+sleep 10;
+#Run migrations
+python manage.py runserver 0.0.0.0:8000
